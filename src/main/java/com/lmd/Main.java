ffdfb85a9d24babb9cdf56ede3c60c32a2166a49
heimo <heimo@gmail.com>
2026-04-22 20:10:34 +0800

    commit05

--- src/main/java/com/lmd/Main.java
+++ src/main/java/com/lmd/Main.java
@@ -4,11 +4,6 @@ package com.lmd;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.println("Hello Git!");
-        System.out.println("Hello Git!");
-        System.out.println("Hello Git!");
         System.out.println("Hello Git!");
         System.out.println("master test");
         System.out.println("hot-fix test");
